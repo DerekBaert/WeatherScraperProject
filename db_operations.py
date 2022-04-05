93bdc698393aef1049eff554e272f5b7a6eee7fc
TySaj <saj.tyler@gmail.com>
2022-04-05 16:49:37 -0500

    April 5, 2022 - EVERYTHING WORKS - Comments are done - Proper looping - Only adding unique data to the database

--- db_operations.py
+++ db_operations.py
@@ -1,39 +1,87 @@
-"""Module level docstring"""
+"""Handles all database queries for the weather database."""
 
 import sqlite3
+from datetime import datetime
 from dbcm import DBCM
 
 class DBOperations():
-    """Class level docstring"""
+    """Class for creating, updating, deleting from the database."""
 
     def initialize_db(self):
-        """Function level docstring"""
-        with DBCM("weather.sqlite") as curs:
-            curs.execute("""create table if not exists sample_data
-                    (id integer primary key autoincrement not null,
-                    sample_date text not null,
-                    location text not null,                   
-                    min_temp real not null,
-                    max_temp real not null,
-                    avg_temp real not null);""")
-    def save_data(self, sample_data):
-        sql = """insert into sample_data (sample_date, location, min_temp, max_temp, avg_temp)
-                values (?, ?, ?, ?, ?)"""
-        location = "Winnipeg, MB"
-        for date, temps in sample_data.items():
-            data = []
-            data.append(date)
-            data.append(location)
-            for key in temps:
-                data.append(temps[key])
+        """Initialize the database."""
+        try:
             with DBCM("weather.sqlite") as curs:
-                curs.execute(sql, data)
+                curs.execute("""create table if not exists sample_data
+                        (id integer primary key autoincrement not null,
+                        sample_date text not null,
+                        location text not null,                   
+                        min_temp real not null,
+                        max_temp real not null,
+                        avg_temp real not null);""")
+        except Exception as error:
+            print("Error: initailize_db: Create database if doesn't exist: ", error)
+    def save_data(self, sample_data):
+        """Adds unique weather data to the database."""
+        try:
+            sql = """insert into sample_data (sample_date, location, min_temp, max_temp, avg_temp)
+                    values (?, ?, ?, ?, ?)"""
+            location = "Winnipeg, MB"
+            date_check_sql = "select max(sample_date) from sample_data"
+        except Exception as error:
+            print("Error: save_data: Initalize sql statements: ", error)
+        try:
+            with DBCM("weather.sqlite") as curs:
+                for row in curs.execute(date_check_sql):
+                    try:
+                        last_date = datetime.strptime(str(row).strip("(),'"), "%Y-%m-%d")
+                    except:
+                        last_date = "None"
+        except Exception as error:
+            print("Error: save_data: Accessing max date: ", error)
+        try:
+            for date, temps in sample_data.items():
+                try:
+                    if last_date == "None" or last_date < datetime.strptime(str(date).strip("'"), "%Y-%m-%d"):
+                        data = []
+                        try:
+                            data.append(date)
+                            data.append(location)  
+                            try:                      
+                                for key in temps:
+                                    data.append(temps[key])
+                            except Exception as error:
+                                print("Error: save_data: Looping through keys: ", error)
+                        except Exception as error:
+                            print("Error: save_data: Adding to database: ", error)
+                        try:
+                            with DBCM("weather.sqlite") as curs:
+                                curs.execute(sql, data)
+                        except Exception as error:
+                            print("Error: save_data: Executing sql to database: ", error)
+                except Exception as error:
+                    print("Error: save_data: Comparing for existing data: ", error)
+        except Exception as error:
+            print("Error: save_data: Looping through data: ", error)
     def purge_data(self):
-        with DBCM("weather.sqlite") as curs:
-            sql = """delete from sample_data"""
-            curs.execute(sql)
+        """Deletes all data in the database."""
+        try:
+            with DBCM("weather.sqlite") as curs:
+                sql = """delete from sample_data"""
+                try:
+                    curs.execute(sql)
+                except Exception as error:
+                    print("Error: purge_data: Executing sql: ", error)
+        except Exception as error:
+            print("Error: purge_data: Accessing database: ", error)
     def fetch_data(self):
-        with DBCM("weather.sqlite") as curs:
-            sql = """select * from sample_data"""
-            for row in curs.execute(sql):
-                print(row)
+        """Prints each row of the database."""
+        try:
+            with DBCM("weather.sqlite") as curs:
+                sql = """select * from sample_data"""
+                try:
+                    for row in curs.execute(sql):
+                        print(row)
+                except Exception as error:
+                    print("Error: fetch_data: Looping through database: ", error)
+        except Exception as error:
+            print("Error: fetch_data: Accessing database: ", error)
--- scrape_weather.py
+++ scrape_weather.py
@@ -1,5 +1,5 @@
 """
-Scrapes data from https://climate.weather.gc.ca/climate_data/daily_data_e.html?StationID=27174&timeframe=2&StartYear=1840&EndYear=2022&Day=1&Year=2018&Month=3
+Scrapes data from Climate website based on today's date.
 """
 from html.parser import HTMLParser
 from html.entities import name2codepoint
@@ -12,26 +12,26 @@ from db_operations import DBOperations
 
 class WeatherScraper(HTMLParser):
     """
-    Setting class variable to check if parser is in body of HTML
+    Class scrapes the weather data only from the passed url.
     """
     def __init__(self):
         """
         Setting class variable to check if parser is in body of HTML
         """
-        HTMLParser.__init__(self)
-        self.in_row = False
-        self.in_data = False
-        self.row_head = False
-        self.in_row_date = False
-        self.stop = False
-        self.in_body = False
-
-        self.entry_date = ""
-        self.daily_temps = {}
-        self.weather = {}
-        self.count = 0
-                
-
+        try:
+            HTMLParser.__init__(self)
+            self.in_row = False
+            self.in_data = False
+            self.row_head = False
+            self.in_row_date = False
+            self.stop = False
+            self.in_body = False
+            self.entry_date = ""
+            self.daily_temps = {}
+            self.weather = {}
+            self.count = 0
+        except Exception as error:
+            print("Error: Init: Initialize variables: ", error)
     def handle_starttag(self, tag, attrs):
         """
         Checking if the tag attribute to determine if the parser is in the row.
@@ -53,8 +53,7 @@ class WeatherScraper(HTMLParser):
                         try:                                
                             self.entry_date = datetime.strptime(attrs[0][1],'%B %d, %Y')
                         except Exception as e:
-                            print("Error parsing date", e)
-                        
+                            print("Error parsing date", e)                      
         except Exception as error:
             print("Error checking start tag", error)
             
@@ -69,9 +68,6 @@ class WeatherScraper(HTMLParser):
                     self.count = 0
                 if(tag == "td"):
                     self.in_data = False
-            # Moved print statement to be when finished handling the table body.
-            # if(tag == "tbody"):
-            #     pprint.pprint(self.weather)
         except Exception as error:
             print("Error checking end tag", error)
                 
@@ -89,66 +85,98 @@ class WeatherScraper(HTMLParser):
                                 if(self.in_data and self.count < 3):
                                     data = data.strip()
                                     if(data == "M" or data == ""):
-                                        self.add_to_dictionary("M")
-                                        self.missing = False
-                                        self.count = self.count + 1
+                                        try:
+                                            self.add_to_dictionary("M")
+                                            self.missing = False
+                                            self.count = self.count + 1
+                                        except Exception as error:
+                                            print("Error: handle_data: Handling missing data: ", error)
                                     elif(data == "LegendM" or data == "LegendE" or data == "E"):
                                         pass
                                     else:
-                                        self.add_to_dictionary(data)
-                                        self.count = self.count + 1
-                                ## Added check to see if daily_temps was empty before adding to weather dictionary.
+                                        try:
+                                            self.add_to_dictionary(data)
+                                            self.count = self.count + 1
+                                        except Exception as error:
+                                            print("Error: handle_data: Handling existing data: ", error)
                                 elif(self.count == 3 and self.daily_temps):
-                                    self.weather[self.entry_date.strftime('%Y-%m-%d')] = self.daily_temps
-                                    self.daily_temps = {}
+                                    try:
+                                        self.weather[self.entry_date.strftime('%Y-%m-%d')] = self.daily_temps
+                                        self.daily_temps = {}
+                                    except Exception as error:
+                                        print("Error: handle_data: Adding data to weather dictionary: ", error)
         except Exception as error:
             print("Error checking or printing data", error)
     def add_to_dictionary(self, data):
-        match self.count:
-            case 0:
-               self.daily_temps['Max'] = data
-            case 1:
-                self.daily_temps['Min'] = data 
-            case 2:
-                self.daily_temps['Mean'] = data
+        """Add data to weather dictionary based on count."""
+        try:
+            match self.count:
+                case 0:
+                    try:
+                        self.daily_temps['Max'] = data
+                    except Exception as error:
+                        print("Error: add_to_dictionary: Adding max: ", error)
+                case 1:
+                    try:
+                        self.daily_temps['Min'] = data
+                    except Exception as error:
+                        print("Error: add_to_dictionary: Adding min: ", error)
+                case 2:
+                    try:
+                        self.daily_temps['Mean'] = data
+                    except Exception as error:
+                        print("Error: add_to_dictionary: Adding mean: ", error)
+        except Exception as error:
+            print("Error: add_to_dictionary: Matching count: ", error)
     def compare_url(self, url_month):
-        if self.entry_date:
-            if url_month != self.entry_date.month:
-                return False
+        """Compare the url month to the month in the attribute tag to determine when to stop scraping."""
+        try:
+            if self.entry_date:
+                try:
+                    if url_month != self.entry_date.month:
+                        return False
+                    else:
+                        return True
+                except Exception as error:
+                    print("Error: compare_url: Checking if months are the same: ", error)
+        except Exception as error:
+            print("Error: compare_url: Checking if entry_date exists: ", error)
+
+try:
+    today = datetime.today()
+    year = today.year
+    month = today.month
+    end_check = True
+    weather = {}
+except Exception as error:
+    print("Error: main: Initializing variables: ", error)
+
+try:
+    while end_check:
+        print(f"Year: {year} Month: {month}")
+        myscraper = WeatherScraper()
+        try:
+            with urllib.request.urlopen(f'https://climate.weather.gc.ca/climate_data/daily_data_e.html?StationID=27174&timeframe=2&StartYear=1840&EndYear=2022&Day=1&Year={year}&Month={month}') as response:
+                html = str(response.read())
+            myscraper.feed(html)
+            weather = weather | myscraper.weather
+            if myscraper.weather:
+                end_check = myscraper.compare_url(month)
+            if (month == 1):
+                year = year - 1
+                month = 12
             else:
-                return True
+                month = month - 1
+                    
+        except Exception as error:
+            print("Error parsing HTML", error)
+except Exception as error:
+    print("Error: main: Checking while loop: ", error)
 
-today = datetime.today()
-year = today.year
-month = today.month
-end_check = True
-weather = {}
-
-month = 12
-year = 1997
-
-while end_check:
-    print(f"Year: {year} Month: {month}")
-    myscraper = WeatherScraper()
-    try:
-        with urllib.request.urlopen(f'https://climate.weather.gc.ca/climate_data/daily_data_e.html?StationID=27174&timeframe=2&StartYear=1840&EndYear=2022&Day=1&Year={year}&Month={month}') as response:
-            html = str(response.read())
-        myscraper.feed(html)
-        weather = weather | myscraper.weather
-        if myscraper.weather:
-            end_check = myscraper.compare_url(month)
-        if (month == 1):
-            year = year - 1
-            month = 12
-        else:
-            month = month - 1
-                
-    except Exception as error:
-        print("Error parsing HTML", error)
-
-pprint.pprint(weather)
-
-db = DBOperations
-db.initialize_db
-db.save_data(db, weather)
-db.fetch_data
+try:
+    db = DBOperations()
+    db.initialize_db()
+    db.save_data(weather)
+    db.fetch_data()
+except Exception as error:
+    print("Error: main: Database calls: ", error)
