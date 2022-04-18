be104f985e5f32554c5629163ceb72b52c8991fe
DerekBaert <derek.baert@gmail.com>
2022-04-18 11:36:32 -0500

    Update weather_processor.py

--- weather_processor.py
+++ weather_processor.py
@@ -36,6 +36,7 @@ class WeatherProcessor():
             case _:
                 print("Error: Invalid entry, please enter a valid selection.")
                 self.user_choice()
+                
     def update_weather(self):
         """
         Updates the database with any missing dates from the website.
@@ -45,28 +46,34 @@ class WeatherProcessor():
         month = today.month
         last_day = self.db.last_day()        
         end_check = True
-        weather = {}        
+        weather = {}     
+
         while end_check:
             print(f"Year: {year} Month: {month}")
             scraper = scrape_weather.WeatherScraper()
+
             with urllib.request.urlopen(f'https://climate.weather.gc.ca/climate_data/daily_data_e.html?StationID=27174&timeframe=2&StartYear=1840&EndYear=2022&Day=1&Year={year}&Month={month}') as response:
                 html = str(response.read())
             scraper.feed(html)
             weather = weather | scraper.weather
+
             if (month == 1):
                 year = year - 1
                 month = 12
             else:
                 month = month - 1
+
             if(scraper.compare_url(last_day.month, last_day.year)):
                 end_check = False
         self.db.initialize_db()
         self.db.save_data(weather)
         print("Up to date.")
+
     def download_weather(self):
         """
         Purges database and repopulates with data from site.
         """
+
         self.db.purge_data()
         today = datetime.today()
         year = today.year
@@ -74,25 +81,32 @@ class WeatherProcessor():
         end_check = True
         weather = {}
         while end_check:
+
             print(f"Year: {year} Month: {month}")
             scraper = scrape_weather.WeatherScraper()
+            
             with urllib.request.urlopen(f'https://climate.weather.gc.ca/climate_data/daily_data_e.html?StationID=27174&timeframe=2&StartYear=1840&EndYear=2022&Day=1&Year={year}&Month={month}') as response:
                 html = str(response.read())
             scraper.feed(html)
             weather = weather | scraper.weather
+
             if scraper.weather:
                 end_check = scraper.compare_url(month, year)
+
             if (month == 1):
                 year = year - 1
                 month = 12
             else:
                 month = month - 1
+
         self.db.initialize_db()
         self.db.save_data(weather)
+
     def generate_box_plot(self):
         """
         Generates a Box Plot from the given range.
         """
+
         print("Box Plot Generator.")
         range = []
         # Need to handle incorrect input
@@ -102,10 +116,12 @@ class WeatherProcessor():
         range.append(end_year)
         weather = self.db.fetch_box_data(range)
         self.plot.box_plot(weather)
+
     def generate_line_plot(self):
         """
         Generates a Line Plot from the given range.
         """
+
         print("Line Plot Generator.")
         # Need to handle incorrect input
         month = input("Enter a month between 1-12: ")
