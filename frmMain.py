45f5933ad27bfe3f8dfee388d92a2836e6abe1b5
DerekBaert <derek.baert@gmail.com>
2022-04-22 23:04:57 -0500

    Created files to start

---
+++ frmMain.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+###########################################################################
+## Python code generated with wxFormBuilder (version 3.10.1-0-g8feb16b3)
+## http://www.wxformbuilder.org/
+##
+## PLEASE DO *NOT* EDIT THIS FILE!
+###########################################################################
+
+import wx
+import wx.xrc
+
